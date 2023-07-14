ffee8a57e633d871e5b5a6f874505850202a5384
Yash Kamble <112558970+yash-devop@users.noreply.github.com>
2023-07-14 12:39:17 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import FlatCards from './components/FlatCards'
+import ElevatedCards from './components/ElevatedCards'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards/>
+        <ElevatedCards/>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
